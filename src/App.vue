c274b1d6b2e17a9f126ded7066744304158670bb
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-08-19 23:42:40 +0200

    🎏 Adding a global style file

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+<script>
+
+</script>  
+
 <template>
   <main id="app">
     <router-view/>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
     <router-link to="/result">Vers le résultat</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
