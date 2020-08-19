d0670817835c730dec1c1d99673a3ecc7e87323b
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-08-19 15:31:31 +0200

    Reorder

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-  <div id="app">
+  <main id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </main>
 </template>
 
 <style lang="stylus">
 #app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+  font-family: Avenir,sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
 </style>
