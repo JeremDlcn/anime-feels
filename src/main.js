aebf746cd9d92c48da727389e14080ee684b8d7e
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-08-23 00:29:54 +0200

    📦 Adding v-scroll-lock dependencies

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import VScrollLock from 'v-scroll-lock'
 
 Vue.config.productionTip = false
+
+Vue.use(VScrollLock)
 
 new Vue({
   store,
