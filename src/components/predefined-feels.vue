d1aea39d2157fc6f25f9fd8d83f5a2f0a4a8f838
JeremDlcrn <44464165+JeremDlcrn@users.noreply.github.com>
2020-08-25 12:40:12 +0200

    🚧 Link predefined to search

--- src/components/predefined-feels.vue
+++ src/components/predefined-feels.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="predefined">
-    <div v-for="category of categories" :key="category.id" :style="{ backgroundImage: 'url(' + category.src + ')'}">
+    <div v-for="category of categories" :key="category.id" @click="()=>$router.push(`result?keywords=${category.name.toLowerCase()}`)" :style="{ backgroundImage: 'url(' + category.src + ')'}">
       <p class="predefined-title">
         {{ category.name }}
       </p>
